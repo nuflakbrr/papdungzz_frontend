fb7f0b7626f6cdd5cfe0cb545ea47878c79669f9
Naufal Akbar <kbrnugroho@gmail.com>
2022-02-13 13:30:26 +0700

    prepare to deploy🚀

--- src/components/Login.js
+++ src/components/Login.js
@@ -9,23 +9,19 @@ import Logo from '../assets/PAPlogo.png'
 import { client } from '../client'
 
 const Login = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const responseGoogle = (response) => {
     localStorage.setItem('user', JSON.stringify(response.profileObj))
-
     const { name, googleId, imageUrl } = response.profileObj
-
     const doc = {
       _id: googleId,
       _type: 'user',
       userName: name,
       image: imageUrl,
-    }
-
-    client.createIfNotExists(doc)
-      .then(() => {
-        navigate('/', { replace: true })
-      })
+    };
+    client.createIfNotExists(doc).then(() => {
+      navigate('/', { replace: true })
+    })
   }
 
   return (
